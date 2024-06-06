ca0ed13188587a5927dff44571119b769be27f41
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2024-06-06 14:13:12 +0300

    update model

---
+++ models/products.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const courseSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  img: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = model('products', courseSchema);
+
+
